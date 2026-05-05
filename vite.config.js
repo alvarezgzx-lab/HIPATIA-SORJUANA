3ce16101b62fcc287ebfc6b9ff433db46709a027
alvarezgzx-lab <jesus.alvarez@regiocontry.edu.mx>
2026-05-05 17:56:00 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // ⚠ cambia esto por el nombre exacto de tu repo en GitHub
   base: '/HIPATIA-SORJUANA/',
 })
